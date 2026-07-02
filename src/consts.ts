8312c695910c87886b5d852ee81741d4464b2657
reneblanco <reneblanco22@gmail.com>
2026-07-02 14:23:10 -0600

    Redesign blog frontend

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Memoblog';
+export const SITE_DESCRIPTION =
+	'Un espacio editorial para compartir historias, proyectos e ideas de impacto social.';
